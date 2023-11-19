3a789fefd05827f30e19df0a58e2f27fbb72717e
Christian Eddie <122130586+Eddie1288@users.noreply.github.com>
2023-11-18 20:18:49 -0700

    Add files via upload

---
+++ Chris/java/com/example/purchasepal/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.purchasepal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
+
+        //admin and admin
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
